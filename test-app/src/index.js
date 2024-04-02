412d8fda70d5099cc5783600e44ac93e6a0cf981
evolentDelight <evolentDelight@outlook.com>
2024-04-02 11:50:22 -0700

    remove unnecessary code

--- test-app/src/index.js
+++ test-app/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 //import App from './App';
 //import Dashboard from "./pages/Dashboard"
-import reportWebVitals from './reportWebVitals';
-import MainNavBar from './components/mainNavBar/mainNavBar.js';
+//import reportWebVitals from './reportWebVitals';
+import MainNavBar from "./components/mainNavBar/mainNavBar.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <MainNavBar />
@@ -22,4 +22,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
